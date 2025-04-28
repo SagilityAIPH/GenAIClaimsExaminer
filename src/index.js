eaaa1cb29f7591aa39e2d15c0f89877e9a365b5a
mjcarag <michaeljohn_1992@yahoo.com>
2025-04-28 16:03:36 -0400

    updated indexjs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';  // <-- ADD THIS
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/SagilityAIPH">    {/* <-- Put your repo name here */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
