b6ec9b9685c0366f1c738ac3813d806ad3474723
mjcarag <michaeljohn_1992@yahoo.com>
2025-04-29 14:25:28 -0400

    updated pdf workers worker.js

--- src/pdfworker.js
+++ src/pdfworker.js
@@ -1,3 +1,4 @@
 import { pdfjs } from 'react-pdf';
  
-pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
+// pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
+pdfjs.GlobalWorkerOptions.workerSrc = process.env.PUBLIC_URL + '/pdf.worker.min.js';
