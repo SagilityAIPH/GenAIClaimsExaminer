bf242eb5c782d1cb89669b487e7ab21b253db486
mjcarag <michaeljohn_1992@yahoo.com>
2025-05-20 11:44:07 -0400

    remove router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
-
 import WordToPDFViewer from './components/WordToPDFViewer';
 
 function App() {
   return (
-    <>
-    <Router basename="/GenAIClaimsExaminer">
+    // <Router basename="/GenAIClaimsExaminer">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/viewer" element={<WordToPDFViewer />} />
-        {/* Add more routes as needed */}
       </Routes>
-    </Router>
-    </>
+    // </Router>
   );
 }
 
